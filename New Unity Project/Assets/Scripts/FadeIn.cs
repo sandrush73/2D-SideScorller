4ef60ed2b066f84eb2a679a1bdac5bd775ef4691
Rusheel  Sandri <recca73@Rusheels-MacBook-Pro.local>
2017-10-02 23:09:26 -0700

    Adding basic player movements and animations

---
+++ New Unity Project/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+	private Image fadePanel;
+	public float fadeInTime = 2f;
+	private Color currentColor = Color.black;
+
+	// Use this for initialization
+	void Start () {
+	
+		fadePanel = GetComponent<Image> ();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Time.timeSinceLevelLoad < fadeInTime) {
+		
+			float althaChage = Time.deltaTime / fadeInTime;
+			currentColor.a -= althaChage;
+			fadePanel.color = currentColor;
+		}else {
+			gameObject.SetActive (false);
+		
+		}
+
+
+
+
+	}
+
+
+}
